edce3a8ec21232132bcef38763d0260b459d934e
Victor Molina <tomasmolinaarias12@gmail.com>
2024-11-08 17:30:02 -0300

    segunda pagina de RESULTADOS

--- app/src/main/java/com/example/prueba2/MainActivity.java
+++ app/src/main/java/com/example/prueba2/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.prueba2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etRut, etNombre, etApellido, etFecha;
+    private Button btnEnviar, btnCerrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        referencias();
+    }
+    private void referencias(){
+        etRut = findViewById(R.id.etRut);
+        etNombre = findViewById(R.id.etNombre);
+        etApellido = findViewById(R.id.etApellido);
+        etFecha = findViewById(R.id.etFecha);
+        btnEnviar = findViewById(R.id.btnEnviar);
+        btnCerrar = findViewById(R.id.btnCerrar);
     }
 }
